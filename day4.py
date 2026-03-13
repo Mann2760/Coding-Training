c27c30922d76abd7e6a3e9925a50f1cc31612c76
Mann2760 <parekhmann182@gmail.com>
2026-03-13 11:53:45 +0530

    Updated day 4 changes

--- day4.py
+++ day4.py
@@ -67,8 +67,14 @@
 #         print(chr(64 + i), end = " ")
 #     print()
 
+# n = int(input("Enter number: "))
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(n + 1 - i, end = " ")
+#     print()
+
 n = int(input("Enter number: "))
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        print(n + 1 - i, end = " ")
+    for j in range(1, n+2-i):
+        print("*", end = " ")
     print()
