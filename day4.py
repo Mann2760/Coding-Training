f2cfd49a065b7864a6554eccdddd8a7e87c52ced
Mann2760 <parekhmann182@gmail.com>
2026-03-13 11:46:43 +0530

    Updated day 4 changes

--- day4.py
+++ day4.py
@@ -55,3 +55,20 @@
 #         print("Not Anagram")
 # else:
 #     print("Not Anagram")
+
+# for i in range(1, 4):
+#     for j in range(1,4):
+#         print(i, end = " ")
+#     print()
+
+# n = int(input("Enter number: "))
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(chr(64 + i), end = " ")
+#     print()
+
+n = int(input("Enter number: "))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(n + 1 - i, end = " ")
+    print()
